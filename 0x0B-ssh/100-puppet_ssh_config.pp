8977317746030521faa6e662f0717123adfa1c76
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-08-15 11:49:07 -0500

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,5 @@
+# Client configuration file (w/ Puppet)
+exec { 'ssh_config':
+  path    => '/bin',
+  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config',
+}
